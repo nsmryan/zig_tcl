599ce2553115c0cc63f4968e91d46b9f39ec0678
Noah Ryan <nsmryan@gmail.com>
2022-07-31 10:58:30 -0400

    Some work on building for Linux

--- build.zig
+++ build.zig
@@ -9,15 +9,26 @@ pub fn build(b: *std.build.Builder) void {
     const lib = b.addSharedLibrary("zigexample", "examples/example.zig", b.version(0, 1, 0));
     lib.setBuildMode(mode);
     lib.linkLibC();
-    lib.addLibPath("c:/tcltk/bin");
-    lib.addLibPath("c:/tcltk/lib");
+
+    if (builtin.os.tag == builtin.Os.windows) {
+        lib.addLibPath("c:/tcltk/bin");
+        lib.addLibPath("c:/tcltk/lib");
+        lib.addIncludeDir("c:/tcltk/include");
+
+        // Stubs does not work- can't seem to get Zig to pick up the .a archive file.
+        // Building with zig itself also doesn't work- some issue with 'zig cc'
+        // Building with MagicSplat's tclstub86.lib file also doesn't work.
+        //lib.linkSystemLibraryName("tclstub86");
+        lib.linkSystemLibraryName("tcl86");
+    } else {
+        lib.addLibPath("/usr/lib");
+        lib.addIncludeDir("/usr/include");
+        // TODO try building with stubs on Linux- much more likely to work.
+        lib.linkSystemLibraryName("tcl8.6");
+        //lib.linkSystemLibraryName("tclstub8.6");
+    }
     lib.addPackagePath("zigtcl", "src/zigtcl.zig");
 
-    // Stubs does not work- can't seem to get Zig to pick up the .a archive file.
-    // Building with zig itself also doesn't work- some issue with 'zig cc'
-    // Building with MagicSplat's tclstub86.lib file also doesn't work.
-    //lib.linkSystemLibraryName("tclstub86");
-    lib.linkSystemLibraryName("tcl86");
     lib.install();
 
     const main_tests = b.addTest("src/zigtcl.zig");
